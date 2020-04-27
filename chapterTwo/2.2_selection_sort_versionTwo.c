b553c936b08286b02d9b42a439730f73cc013e84
sumedh105 <sumedh105@gmail.com>
2020-04-27 12:54:55 +0200

    Program to implement a selection sort in C

---
+++ chapterTwo/2.2_selection_sort_versionTwo.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#define MAXNUM 10
+
+static void selection_sort(int arr[], int num);
+
+int main()
+{
+	int num = 0;
+	int arr[MAXNUM] = {0};
+	int index = 0;
+
+	printf("\nEnter the number of elements that needs to be inserted in array\n");
+	scanf("%d", &num);
+	printf("\nThe number of elements that needs to be inserted in an array are: %d\n", num);
+
+	printf("\nEnter the elements in an array that needs to be sorted\n");
+	for (index = 0; index < num; ++index)
+	{
+		scanf("%d", &arr[index]);
+	}
+
+	printf("\nThe array before sorting is:\n");
+	for (index = 0; index < num; ++index)
+	{
+		printf("\n%d\n", arr[index]);
+	}
+
+	selection_sort(arr, num);
+
+	printf("\nThe sorted array is:\n");
+	for (index = 0; index < num; ++index)
+	{
+		printf("\n%d\n", arr[index]);
+	}
+
+	return 0;
+}
+
+static void selection_sort(int arr[], int num)
+{
+	int outer_index = 0;
+	int inner_index = 0;
+	int pos = 0;
+	int min = 0;
+	int temp = 0;
+
+	for (outer_index = 0; outer_index < num; ++outer_index)
+	{
+		min = arr[outer_index];
+		pos = outer_index;
+		for (inner_index = outer_index + 1; inner_index < num; ++inner_index)
+		{
+			if (arr[inner_index] < min)
+			{
+				min = arr[inner_index];
+				pos = inner_index;
+			}
+		}
+		if (pos != outer_index)
+		{
+			temp = arr[outer_index];
+			arr[outer_index] = min;
+			arr[pos] = temp;
+		}
+	}
+}
